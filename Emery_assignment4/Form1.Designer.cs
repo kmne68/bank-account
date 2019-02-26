35c41d36b8958be486b87882084cf92b98ea67a2
Keith Emery <kmne68@gmail.com>
2019-02-25 23:13:03 -0600

    Add project files.

---
+++ Emery_assignment4/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Emery_assignment4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cb_accountTypes = new System.Windows.Forms.ComboBox();
+            this.tb_fName = new System.Windows.Forms.TextBox();
+            this.tb_lName = new System.Windows.Forms.TextBox();
+            this.tb_withdraw = new System.Windows.Forms.TextBox();
+            this.tb_deposit = new System.Windows.Forms.TextBox();
+            this.btn_welcome = new System.Windows.Forms.Button();
+            this.btn_deposit = new System.Windows.Forms.Button();
+            this.btn_withdraw = new System.Windows.Forms.Button();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.btn_print = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.mtb_accountNumber = new System.Windows.Forms.MaskedTextBox();
+            this.gb_customerInfo = new System.Windows.Forms.GroupBox();
+            this.gb_customerInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Account:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 88);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Customer Acct #:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 133);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "First Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 184);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Last Name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(555, 39);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Transaction:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(568, 111);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(176, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Enter Amount to Withdraw:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(568, 178);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(166, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Enter Amount to Deposit:";
+            // 
+            // cb_accountTypes
+            // 
+            this.cb_accountTypes.FormattingEnabled = true;
+            this.cb_accountTypes.Location = new System.Drawing.Point(117, 32);
+            this.cb_accountTypes.Margin = new System.Windows.Forms.Padding(4);
+            this.cb_accountTypes.Name = "cb_accountTypes";
+            this.cb_accountTypes.Size = new System.Drawing.Size(229, 24);
+            this.cb_accountTypes.TabIndex = 8;
+            // 
+            // tb_fName
+            // 
+            this.tb_fName.Location = new System.Drawing.Point(139, 124);
+            this.tb_fName.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_fName.Name = "tb_fName";
+            this.tb_fName.Size = new System.Drawing.Size(132, 22);
+            this.tb_fName.TabIndex = 10;
+            // 
+            // tb_lName
+            // 
+            this.tb_lName.Location = new System.Drawing.Point(139, 184);
+            this.tb_lName.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_lName.Name = "tb_lName";
+            this.tb_lName.Size = new System.Drawing.Size(132, 22);
+            this.tb_lName.TabIndex = 11;
+            // 
+            // tb_withdraw
+            // 
+            this.tb_withdraw.Location = new System.Drawing.Point(757, 101);
+            this.tb_withdraw.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_withdraw.Name = "tb_withdraw";
+            this.tb_withdraw.Size = new System.Drawing.Size(132, 22);
+            this.tb_withdraw.TabIndex = 12;
+            // 
+            // tb_deposit
+            // 
+            this.tb_deposit.Location = new System.Drawing.Point(757, 178);
+            this.tb_deposit.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_deposit.Name = "tb_deposit";
+            this.tb_deposit.Size = new System.Drawing.Size(132, 22);
+            this.tb_deposit.TabIndex = 13;
+            // 
+            // btn_welcome
+            // 
+            this.btn_welcome.Location = new System.Drawing.Point(143, 357);
+            this.btn_welcome.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_welcome.Name = "btn_welcome";
+            this.btn_welcome.Size = new System.Drawing.Size(100, 28);
+            this.btn_welcome.TabIndex = 14;
+            this.btn_welcome.Text = "Welcome";
+            this.btn_welcome.UseVisualStyleBackColor = true;
+            this.btn_welcome.Click += new System.EventHandler(this.btn_welcome_Click);
+            // 
+            // btn_deposit
+            // 
+            this.btn_deposit.Location = new System.Drawing.Point(251, 357);
+            this.btn_deposit.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_deposit.Name = "btn_deposit";
+            this.btn_deposit.Size = new System.Drawing.Size(100, 28);
+            this.btn_deposit.TabIndex = 15;
+            this.btn_deposit.Text = "Desposit";
+            this.btn_deposit.UseVisualStyleBackColor = true;
+            this.btn_deposit.Click += new System.EventHandler(this.btn_Deposit_Click);
+            // 
+            // btn_withdraw
+            // 
+            this.btn_withdraw.Location = new System.Drawing.Point(359, 357);
+            this.btn_withdraw.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_withdraw.Name = "btn_withdraw";
+            this.btn_withdraw.Size = new System.Drawing.Size(100, 28);
+            this.btn_withdraw.TabIndex = 16;
+            this.btn_withdraw.Text = "Withdrawal";
+            this.btn_withdraw.UseVisualStyleBackColor = true;
+            this.btn_withdraw.Click += new System.EventHandler(this.btn_withdraw_Click);
+            // 
+            // btn_show
+            // 
+            this.btn_show.Location = new System.Drawing.Point(467, 357);
+            this.btn_show.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(100, 28);
+            this.btn_show.TabIndex = 17;
+            this.btn_show.Text = "Show Balance";
+            this.btn_show.UseVisualStyleBackColor = true;
+            this.btn_show.Click += new System.EventHandler(this.btn_show_Click);
+            // 
+            // btn_print
+            // 
+            this.btn_print.Location = new System.Drawing.Point(575, 357);
+            this.btn_print.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_print.Name = "btn_print";
+            this.btn_print.Size = new System.Drawing.Size(100, 28);
+            this.btn_print.TabIndex = 18;
+            this.btn_print.Text = "Print Statement";
+            this.btn_print.UseVisualStyleBackColor = true;
+            this.btn_print.Click += new System.EventHandler(this.btn_print_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(683, 357);
+            this.btn_clear.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(100, 28);
+            this.btn_clear.TabIndex = 19;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(791, 357);
+            this.btn_close.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(100, 28);
+            this.btn_close.TabIndex = 20;
+            this.btn_close.Text = "Exit";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // mtb_accountNumber
+            // 
+            this.mtb_accountNumber.Location = new System.Drawing.Point(139, 83);
+            this.mtb_accountNumber.Mask = "000000";
+            this.mtb_accountNumber.Name = "mtb_accountNumber";
+            this.mtb_accountNumber.Size = new System.Drawing.Size(132, 22);
+            this.mtb_accountNumber.TabIndex = 21;
+            this.mtb_accountNumber.ValidatingType = typeof(int);
+            // 
+            // gb_customerInfo
+            // 
+            this.gb_customerInfo.Controls.Add(this.cb_accountTypes);
+            this.gb_customerInfo.Controls.Add(this.mtb_accountNumber);
+            this.gb_customerInfo.Controls.Add(this.label2);
+            this.gb_customerInfo.Controls.Add(this.label3);
+            this.gb_customerInfo.Controls.Add(this.tb_fName);
+            this.gb_customerInfo.Controls.Add(this.label4);
+            this.gb_customerInfo.Controls.Add(this.tb_lName);
+            this.gb_customerInfo.Controls.Add(this.label5);
+            this.gb_customerInfo.Location = new System.Drawing.Point(143, 39);
+            this.gb_customerInfo.Name = "gb_customerInfo";
+            this.gb_customerInfo.Size = new System.Drawing.Size(362, 250);
+            this.gb_customerInfo.TabIndex = 22;
+            this.gb_customerInfo.TabStop = false;
+            this.gb_customerInfo.Text = "Customer Information";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.gb_customerInfo);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_print);
+            this.Controls.Add(this.btn_show);
+            this.Controls.Add(this.btn_withdraw);
+            this.Controls.Add(this.btn_deposit);
+            this.Controls.Add(this.btn_welcome);
+            this.Controls.Add(this.tb_deposit);
+            this.Controls.Add(this.tb_withdraw);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gb_customerInfo.ResumeLayout(false);
+            this.gb_customerInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cb_accountTypes;
+        private System.Windows.Forms.TextBox tb_fName;
+        private System.Windows.Forms.TextBox tb_lName;
+        private System.Windows.Forms.TextBox tb_withdraw;
+        private System.Windows.Forms.TextBox tb_deposit;
+        private System.Windows.Forms.Button btn_welcome;
+        private System.Windows.Forms.Button btn_deposit;
+        private System.Windows.Forms.Button btn_withdraw;
+        private System.Windows.Forms.Button btn_show;
+        private System.Windows.Forms.Button btn_print;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.MaskedTextBox mtb_accountNumber;
+        private System.Windows.Forms.GroupBox gb_customerInfo;
+    }
+}
+
---
+++ Emery_assignment4/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Emery_assignment4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateCombo();
+
+            btn_deposit.Enabled     = false;
+            btn_withdraw.Enabled    = false;
+            btn_show.Enabled        = false;
+            btn_print.Enabled       = false;
+            tb_deposit.Enabled      = false;
+            tb_withdraw.Enabled     = false;
+            
+//            cb_accountTypes.SelectedIndex = 0;
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_show_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        public bool IsPresent(TextBox textBox, string name)
+        {
+            if(textBox.Name == "tb_fName" && textBox.Text == "")
+            {
+                MessageBox.Show("Customer's First Name is blank!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Focus();
+                return false;
+            }
+            if (textBox.Name == "tb_lName" && textBox.Text == "")
+            {
+                MessageBox.Show("Customer's Last Name is blank!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+
+        private void PopulateCombo()
+        {
+            cb_accountTypes.Items.Add("--- Select An Account Type ---");
+            cb_accountTypes.Items.Add("Checking");
+            cb_accountTypes.Items.Add("Savings");
+            cb_accountTypes.Items.Add("Money Market");
+            cb_accountTypes.Items.Add("Certificate of Deposit");
+
+            cb_accountTypes.SelectedIndex = 0;
+        }
+
+
+        private void MethodNotImplemented()
+        {
+            MessageBox.Show("Method implementation pending","", MessageBoxButtons.OK, MessageBoxIcon.Information); ;
+        }
+
+
+        private void btn_welcome_Click(object sender, EventArgs e)
+        {
+
+            int selectedActivity = 0;
+            string accountFirstName = "";
+            string accountLastName = "";
+            try
+            {
+                if (cb_accountTypes.SelectedIndex > 0)
+                {
+                    selectedActivity = cb_accountTypes.SelectedIndex;
+                }
+                else
+                {
+                    MessageBox.Show("No account type was selected!");
+                }
+                int accountNumber = Convert.ToInt32(mtb_accountNumber.Text);
+                Console.WriteLine("accountNumber is " + accountNumber);
+
+                //   int accountNumber = Convert.ToInt16(tb_acctnmbr.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Customer Account Number is blank", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            try
+            {
+                if (IsPresent(tb_fName, "First Name") && IsPresent(tb_lName, "Last Name"))
+                {
+                    accountFirstName = tb_fName.Text;
+                    accountLastName = tb_lName.Text;
+
+                    MessageBox.Show("Customer " + accountFirstName + " " +
+                        accountLastName + " has $500 in the " + cb_accountTypes.SelectedItem.ToString() + " account.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An unknown error has occurred.");
+            }
+
+            EnableButtons();
+            gb_customerInfo.Enabled = false;
+        }
+
+
+        private void btn_Deposit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FutureFeature();
+            }
+            catch (NotImplementedException ex)
+            {
+                MethodNotImplemented();
+            }
+        }
+
+
+        private void btn_withdraw_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FutureFeature();
+            }
+            catch (NotImplementedException ex)
+            {
+                MethodNotImplemented();
+            }
+        }
+
+        private void btn_print_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FutureFeature();
+            }
+            catch (NotImplementedException ex)
+            {
+                MethodNotImplemented();
+            }
+        }
+
+        private void FutureFeature()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            tb_deposit.Text = "";
+            tb_fName.Text = "";
+            tb_lName.Text = "";
+            tb_withdraw.Text = "";
+            mtb_accountNumber.Text = "";
+            cb_accountTypes.Text = "";
+        }
+
+
+        private void EnableButtons()
+        {
+            btn_deposit.Enabled     = true;
+            btn_withdraw.Enabled    = true;
+            btn_show.Enabled        = true;
+            btn_print.Enabled       = true;
+            tb_deposit.Enabled      = true;
+            tb_withdraw.Enabled     = true;
+        }
+
+
+    }
+}
